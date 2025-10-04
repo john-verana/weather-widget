27a858297bfaff45ade5f908b6566aa6bc58596d
john-verana <johnverana@gmail.com>
2025-10-05 05:00:22 +0800

    feat(script.js): intergrate OpenWeather API for real-time weather data

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
+const apiKey = "1df0058054000eefc2fbfe9118721770";
 document.addEventListener("DOMContentLoaded", () => {
     const el = document.getElementById("weather-0");
 
-    const cities = [
-        {name: "Las Pinas, PH", temp: 29, condition: "Cloudy", icon: "☁️" },
-        {name: "Paranaque, PH", temp: 31, condition: "Sunny", icon: "☀️" },
-        {name: "Cavite, PH", temp: 27, condition: "Rain", icon: "🌧️" },
-    ];
-
+    const cities = ["Las Pinas,PH", "Paranaque, PH", "Cavite, PH"];
     let i = 0;
 
-    function render(city) {
-        el.textContent =`${city.name}: ${city.temp}: ${city.condition}: ${city.icon}`;
+    async function render(cityName) {
+        el.textContent = "Loading...";
+        try {
+            const res = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`
+            );
+            const data = await res.json();
+
+            const temp = Math.round(data.main.temp);
+            const condition = data.weather[0].main;
+            const icon = data.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
+
+            el.innerHTML = `
+            <div>
+          <img src="${iconUrl}" alt="${condition}">
+          <p>${data.name}: ${temp}°C · ${condition}</p>
+        </div>
+        `;
+        } catch (error) {
+            el.textContent = "Error loading data";
+            console.error(error);
+        }
     }
 
     render(cities[i]);
 
+    // Rotate every 5 seconds 
     setInterval(() => {
         i = (i + 1) % cities.length;
         render(cities[i]);
-    }, 5000);
+    }, 3000);
 });
